773cc821ddf8fc74f28efd855f892290a8370426
Celosz-fle7 <celoszfle7@gmail.com>
2026-05-28 17:03:09 +0700

    add tv focus navigation polish

--- lib/shared/widgets/poster_card.dart
+++ lib/shared/widgets/poster_card.dart
@@ -2,21 +2,56 @@ import 'package:flutter/material.dart';
 import '../../core/app_theme.dart';
 import '../../models/content_item.dart';
 
-class PosterCard extends StatelessWidget {
+class PosterCard extends StatefulWidget {
   final ContentItem item;
   final VoidCallback? onTap;
   final bool tv;
 
-  const PosterCard({super.key, required this.item, this.onTap, this.tv = false});
+  const PosterCard({
+    super.key,
+    required this.item,
+    this.onTap,
+    this.tv = false,
+  });
+
+  @override
+  State<PosterCard> createState() => _PosterCardState();
+}
+
+class _PosterCardState extends State<PosterCard> {
+  bool _focused = false;
 
   @override
   Widget build(BuildContext context) {
-    final width = tv ? 158.0 : 150.0;
-    return InkWell(
-      onTap: onTap,
-      borderRadius: BorderRadius.circular(18),
-      child: SizedBox(
+    final width = widget.tv ? 166.0 : 150.0;
+    final card = AnimatedScale(
+      scale: widget.tv && _focused ? 1.08 : 1.0,
+      duration: const Duration(milliseconds: 120),
+      curve: Curves.easeOutCubic,
+      child: AnimatedContainer(
+        duration: const Duration(milliseconds: 120),
         width: width,
+        padding: EdgeInsets.all(widget.tv && _focused ? 3 : 0),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(22),
+          border: Border.all(
+            color: widget.tv && _focused ? AppTheme.cyan : Colors.transparent,
+            width: 2.5,
+          ),
+          boxShadow: widget.tv && _focused
+              ? [
+                  BoxShadow(
+                    color: AppTheme.cyan.withOpacity(0.32),
+                    blurRadius: 24,
+                    spreadRadius: 1,
+                  ),
+                  BoxShadow(
+                    color: AppTheme.purple.withOpacity(0.22),
+                    blurRadius: 36,
+                  ),
+                ]
+              : const [],
+        ),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
@@ -26,38 +61,75 @@ class PosterCard extends StatelessWidget {
                 child: Stack(
                   fit: StackFit.expand,
                   children: [
-                    Image.network(item.posterUrl, fit: BoxFit.cover),
-                    Positioned(
-                      top: 8,
-                      left: 8,
-                      child: _Badge(text: '${item.episodes} Ep'),
-                    ),
-                    if (item.updated)
-                      const Positioned(top: 8, right: 8, child: _Badge(text: 'UPDATE')),
-                    Positioned(
-                      right: 8,
-                      bottom: 8,
-                      child: _Badge(text: item.rating.toStringAsFixed(1)),
-                    ),
+                    widget.item.posterUrl.isEmpty
+                        ? Container(
+                            color: const Color(0xFF111827),
+                            child: const Icon(Icons.movie_creation_rounded, color: Colors.white38),
+                          )
+                        : Image.network(
+                            widget.item.posterUrl,
+                            fit: BoxFit.cover,
+                            errorBuilder: (_, __, ___) => Container(
+                              color: const Color(0xFF111827),
+                              child: const Icon(Icons.broken_image_rounded, color: Colors.white38),
+                            ),
+                          ),
+                    Positioned(top: 8, left: 8, child: _Badge(text: '${widget.item.episodes} Ep')),
+                    if (widget.item.updated) const Positioned(top: 8, right: 8, child: _Badge(text: 'UPDATE')),
+                    Positioned(right: 8, bottom: 8, child: _Badge(text: widget.item.rating.toStringAsFixed(1))),
+                    if (widget.tv && _focused)
+                      Positioned.fill(
+                        child: DecoratedBox(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(18),
+                            gradient: LinearGradient(
+                              begin: Alignment.topCenter,
+                              end: Alignment.bottomCenter,
+                              colors: [
+                                Colors.white.withOpacity(0.08),
+                                AppTheme.cyan.withOpacity(0.08),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
                   ],
                 ),
               ),
             ),
             const SizedBox(height: 8),
             Text(
-              item.title,
+              widget.item.title,
               maxLines: 2,
               textAlign: TextAlign.center,
               overflow: TextOverflow.ellipsis,
               style: TextStyle(
                 color: Colors.white,
-                fontSize: tv ? 14 : 13,
+                fontSize: widget.tv ? 14 : 13,
                 fontWeight: FontWeight.w800,
                 height: 1.1,
               ),
             ),
           ],
         ),
+      ),
+    );
+
+    if (!widget.tv) {
+      return InkWell(
+        onTap: widget.onTap,
+        borderRadius: BorderRadius.circular(18),
+        child: SizedBox(width: width, child: card),
+      );
+    }
+
+    return Focus(
+      onFocusChange: (value) => setState(() => _focused = value),
+      child: InkWell(
+        onTap: widget.onTap,
+        onLongPress: widget.onTap,
+        borderRadius: BorderRadius.circular(22),
+        child: SizedBox(width: width, child: card),
       ),
     );
   }
--- lib/tv/tv_app.dart
+++ lib/tv/tv_app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import '../shared/widgets/premium_shell.dart';
 import 'screens/tv_account_screen.dart';
 import 'screens/tv_home_screen.dart';
@@ -14,8 +15,15 @@ class TvApp extends StatefulWidget {
 }
 
 class _TvAppState extends State<TvApp> {
+  final FocusNode _rootFocus = FocusNode(debugLabel: 'tv-root-focus');
   int index = 0;
   bool navOpen = false;
+
+  @override
+  void dispose() {
+    _rootFocus.dispose();
+    super.dispose();
+  }
 
   Widget _page() {
     return switch (index) {
@@ -28,28 +36,47 @@ class _TvAppState extends State<TvApp> {
     };
   }
 
+  void _handleKey(KeyEvent event) {
+    if (event is! KeyDownEvent) return;
+    final key = event.logicalKey;
+    if (key == LogicalKeyboardKey.arrowLeft) {
+      if (!navOpen) setState(() => navOpen = true);
+    }
+    if (key == LogicalKeyboardKey.escape || key == LogicalKeyboardKey.goBack) {
+      if (navOpen) setState(() => navOpen = false);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: PremiumShell(
-        child: MouseRegion(
-          onEnter: (_) => setState(() => navOpen = true),
-          onExit: (_) => setState(() => navOpen = false),
-          child: Stack(
-            children: [
-              _page(),
-              Align(
-                alignment: Alignment.centerLeft,
-                child: Focus(
-                  onFocusChange: (v) => setState(() => navOpen = v),
-                  child: TvSideNav(
-                    index: index,
-                    expanded: navOpen,
-                    onChanged: (v) => setState(() => index = v),
+    return KeyboardListener(
+      focusNode: _rootFocus,
+      autofocus: true,
+      onKeyEvent: _handleKey,
+      child: Scaffold(
+        body: PremiumShell(
+          child: MouseRegion(
+            onEnter: (_) => setState(() => navOpen = true),
+            onExit: (_) => setState(() => navOpen = false),
+            child: Stack(
+              children: [
+                _page(),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Focus(
+                    onFocusChange: (v) => setState(() => navOpen = v || navOpen),
+                    child: TvSideNav(
+                      index: index,
+                      expanded: navOpen,
+                      onChanged: (v) => setState(() {
+                        index = v;
+                        navOpen = false;
+                      }),
+                    ),
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
